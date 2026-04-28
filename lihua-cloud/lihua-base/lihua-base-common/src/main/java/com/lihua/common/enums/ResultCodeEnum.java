29946d0eb1de6a6258d0adff246d13583cd86544
Yukino <2651518588@qq.com>
2026-04-28 20:56:05 +0800

    请求结果枚举修改

--- lihua-cloud/lihua-base/lihua-base-common/src/main/java/com/lihua/common/enums/ResultCodeEnum.java
+++ lihua-cloud/lihua-base/lihua-base-common/src/main/java/com/lihua/common/enums/ResultCodeEnum.java
@@ -10,7 +10,9 @@ import lombok.Getter;
 @Getter
 public enum ResultCodeEnum {
 
+    // 访问成功
     SUCCESS (200,"成功"),
+    // 请求参数/权限/资源异常，4xx
     PARAMS_ERROR(400,"参数异常"),
     AUTHENTICATION_EXPIRED(401,"身份验证过期，请重新登录"),
     PARAMS_MISSING(402,"参数缺失或不完整"),
@@ -19,17 +21,19 @@ public enum ResultCodeEnum {
     REQUEST_METHOD_ERROR(405,"请求方法异常"),
     ACCESS_EXPIRED_ERROR (406,"请求资源权限过期"),
     IP_ILLEGAL_ERROR(407, "暂时无法为该地区提供服务"),
+    // 后台业务异常，5xx开始
     ERROR (500,"业务异常"),
-    FILE_ERROR (501,"附件处理异常"),
-    BAD_GATEWAY_ERROR (502,"网关异常"),
-    SERVER_BAD_ERROR (503,"服务不可用"),
-    MAX_UPLOAD_SIZE_EXCEEDED_ERROR (504,"上传的附件超过了允许的最大大小限制"),
-    SERVER_UNAVAILABLE (505,"服务器维护中"),
-    CAPTCHA_ERROR(506,"验证码错误"),
-    SENSITIVE_ERROR(507,"数据脱敏异常"),
-    EXCEL_IMPORT_ERROR (510,"Excel导入异常"),
-    EXCEL_EXPORT_ERROR (511,"Excel导出异常"),
-    SYSTEM_ERROR (512,"系统异常");
+    SYSTEM_ERROR (501,"系统异常"),
+    SERVER_BAD_ERROR (502,"服务不可用"),
+    BAD_GATEWAY_ERROR (503,"网关异常"),
+    SERVER_UNAVAILABLE (504,"服务器维护中"),
+    FILE_ERROR (505,"附件处理异常"),
+    MAX_UPLOAD_SIZE_EXCEEDED_ERROR (506,"上传的附件超过了允许的最大大小限制"),
+    CAPTCHA_ERROR(507,"验证码错误"),
+    SENSITIVE_ERROR(508,"数据脱敏异常"),
+    EXCEL_IMPORT_ERROR (509,"Excel导入异常"),
+    EXCEL_EXPORT_ERROR (510,"Excel导出异常");
+
 
     /**
      * 状态码
