5859a0c7b5a3f4222e7849b88b94af9e112ef901
Yukino <2651518588@qq.com>
2026-05-08 16:28:24 +0800

    环境配置

--- lihua-vue/env.d.ts
+++ lihua-vue/env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
-declare module '*.vue' {
-    import type {DefineComponent} from 'vue'
-    import Vue from 'vue'
 
-    const component: DefineComponent<{}, {}, any> | Vue
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+
+    const component: DefineComponent<{}, {}, any>
 
     export default component
 }
